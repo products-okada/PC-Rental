bb0ab1174d4b47b37454931473d64a3f9b219647
ryohei_okada <ryohei.okada@hakuhodo.co.jp>
2021-11-17 16:59:33 +0900

    Revert "Controllerとbladeの追加"
    This reverts commit 64096b0f97653624bfa08cb1b386cf8a7e7a3444.

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\LoginController;
-Route::get('/login',[LoginController::class, 'index']);
-
-use App\Http\Controllers\ListController;
-Route::get('/list',[ListController::class, 'index']);
-
-use App\Http\Controllers\LogController;
-Route::get('/log',[LogController::class, 'index']);
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
