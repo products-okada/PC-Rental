48e386446cde85e6e8833aa4db2140c41e024d31
ryohei_okada <ryohei.okada@hakuhodo.co.jp>
2021-11-18 17:31:56 +0900

    mobile_pcTableの修正

--- database/seeders/MobilePcSeeder.php
+++ database/seeders/MobilePcSeeder.php
@@ -21,31 +21,37 @@ class MobilePcSeeder extends Seeder
             [
             'pcid' => 'Y19MW1018',
             'status' => 1,
+            'user' => '野村',
             'update' => new DateTime(),
             ],
             [
             'pcid' => 'Y19MW1019',
             'status' => 0,
+            'user' => 'Iwasaki',
             'update' => new DateTime(),
             ],
             [
             'pcid' => 'Y19MW1153',
             'status' => 1,
+            'user' => '水野',
             'update' => new DateTime(),
             ],
             [
             'pcid' => 'Y19MW1154',
             'status' => 1,
+            'user' => '福村',
             'update' => new DateTime(),
             ],
             [
             'pcid' => 'Y19MM064',
             'status' => 1,
+            'user' => '岡田',
             'update' => new DateTime(),
             ],
             [
             'pcid' => 'スピーカー',
             'status' => 0,
+            'user' => '野村',
             'update' => new DateTime(),
             ],
             
