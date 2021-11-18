0ffa77981a661d9d85a138d653b48db66e93d63b
ryohei_okada <ryohei.okada@hakuhodo.co.jp>
2021-11-18 15:42:25 +0900

    DBからの値取得＆表示＋UIの調整

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 class LogController extends Controller
 {
     public function index(){
-        $logItems = \DB::table('rental_log')->get();
+        // $logItems = \DB::table('rental_log')->get();
+        $logItems = \DB::table('rental_log')->paginate(50);
         return ( view('log',['logItems' => $logItems]) );
     }
 }
--- resources/views/log.blade.php
+++ resources/views/log.blade.php
@@ -2,34 +2,44 @@
     $status = ["貸出", "返却"];
 @endphp
 
+<!doctype html>
+<html lang="ja">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<link href="{{asset('/css/log/logtable.css')}}" rel="stylesheet">
-<link href="{{asset('/css/log/toListButton.css')}}" rel="stylesheet">
-    <div class="container mx-auto">
-        <h1 class="mb-8">履歴</h1>
-        <a href="http://localhost/laravel/pc_rental/public/list" class="btn btn--orange">貸出画面へ</a>
-
-        <br><br>
-        <table class="text-left w-full">
-            <thead class="bg-black flex text-white w-full">
-                <tr class="flex w-full mb-4">
-                    <th class="p-4 w-1/3">端末ID</th>
-                    <th class="p-4 w-1/3">ステータス</th>
-                    <th class="p-4 w-1/3">ユーザー名</th>
-                    <th class="p-4 w-1/3">更新日時</th>
-                </tr>
-            </thead>
-            <tbody class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style="height: 50vh;">
-            
-                @foreach($logItems as $logItems)
-                    <tr class="flex w-full mb-4">
-                        <td class="p-4 w-1/3">{{ $logItems->pcid }}</a></td>
-                        <td class="p-4 w-1/3">{{ $status[$logItems->status] }}</td>
-                        <td class="p-4 w-1/3">{{ $logItems->user }}</td>
-                        <td class="p-4 w-1/3">{{ $logItems->update }}</td>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <!-- <link href="{{asset('/css/log/logtable.css')}}" rel="stylesheet"> -->
+    <link href="{{asset('/css/log/toListButton.css')}}" rel="stylesheet">   
+    
+</head>
+    <body>
+        <div class="container mx-auto">
+            <h1 class="mb-8">履歴</h1>
+            <a href="http://localhost/laravel/pc_rental/public/list" class="btn btn--orange">貸出画面へ</a>
+            <p style="text-align: right">{{$logItems->links()}}</p>
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th class="p-4 w-1/3">端末ID</th>
+                        <th class="p-4 w-1/3">ステータス</th>
+                        <th class="p-4 w-1/3">ユーザー名</th>
+                        <th class="p-4 w-1/3">更新日時</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
-
+                </thead>
+                <tbody>
+                    @foreach($logItems as $logItem)
+                        <tr>
+                            <td class="p-4 w-1/3">{{ $logItem->pcid }}</a></td>
+                            <td class="p-4 w-1/3">{{ $status[$logItem->status] }}</td>
+                            <td class="p-4 w-1/3">{{ $logItem->user }}</td>
+                            <td class="p-4 w-1/3">{{ $logItem->update }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
